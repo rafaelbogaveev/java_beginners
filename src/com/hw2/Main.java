5aa72e18241d22685fd7d153c36ebd602a89da61
Rafael Bogaveev <bogaveev.rafael@gmail.com>
2024-12-23 22:36:49 +0300

    added hw assignment.

--- src/com/hw2/Main.java
+++ src/com/hw2/Main.java
@@ -5,7 +5,19 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
         int[] numbers = {5, 2, 9, 1, 5, 6};
-        Arrays.sort(numbers);
+        //Arrays.sort(numbers);
+        Sort(numbers);
         System.out.println(Arrays.toString(numbers));
+    }
+
+    public static void Sort(int[] numbers){
+        //5, 2, 9, 1, 5, 6
+        //2, 5, 1, 5, 6, 9
+        //2, 1, 5, 5, 6, 9
+        //1, 2, 5, 5, 6, 9
+    }
+
+    public static int getMin(int[] numbers){
+
     }
 }
